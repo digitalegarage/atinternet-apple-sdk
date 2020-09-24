24705a155ed06a977b81ea0b8b1c4219e41db32e
Arsatius Regler <arsatius@uber.space>
2020-09-24 10:41:46 +0200

    don’t expose crash and stuff

--- ATInternetTracker/Sources/TrackerExtension.h
+++ ATInternetTracker/Sources/TrackerExtension.h
@@ -31,5 +31,5 @@ FOUNDATION_EXPORT double ExtensionTrackerVersionNumber;
 FOUNDATION_EXPORT const unsigned char ExtensionTrackerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Tracker/PublicHeader.h>
-#import "Crash.h"
-#import "Hash.h"
+//#import "Crash.h"
+//#import "Hash.h"
