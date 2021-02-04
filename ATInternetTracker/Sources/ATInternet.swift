69e001f1e6baf4a41085377dd8f9200c947ee6bc
Arsatius Regler <arsatius@uber.space>
2021-02-04 11:21:13 +0100

    If you freaking deprecate a thing, at least leave a message

--- ATInternetTracker/Sources/ATInternet.swift
+++ ATInternetTracker/Sources/ATInternet.swift
@@ -154,8 +154,6 @@ public class ATInternet: NSObject {
         }
     }
     
-    /// Disable user identification.
-    @available(*, deprecated, message: "")
     @objc public class var optOut: Bool {
         get {
             return TechnicalContext.optOut
